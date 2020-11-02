32e36c361c5f96ab43512265aff7219ff4f0b5d3
ava-lu <alu21@stuy.edu>
2020-11-02 15:55:13 -0500

    Added constructor

--- SuperArray.java
+++ SuperArray.java
@@ -2,4 +2,8 @@ public class SuperArray {
   //instance variables
   private String[] data;
   private int size; //the current size
+
+  public SuperArray() {
+    int[] superArray = new int[10];
+  }
 }
