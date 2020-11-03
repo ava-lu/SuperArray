a8304b6d0b7e84eb3df52c01d0d5b614231e3478
ava-lu <alu21@stuy.edu>
2020-11-03 14:08:09 -0500

    added first 4 methods during class

--- SuperArray.java
+++ SuperArray.java
@@ -30,10 +30,41 @@ public class SuperArray {
   }
 
   private void resize() {
-    String[] bigdata = new String[size+10];
+    String[] bigdata = new String[size*2];
     for (int i=0; i<data.length; i++) {
       bigdata[i] = data[i];
     }
     data = bigdata;
+  }
+
+  public boolean isEmpty() {
+    return(size==0);
+  }
+
+  public void clear() {
+    size = 0;
+    for (int i=0; i<data.length; i++) {
+      data[i] = null;
+    }
+  }
+
+   public String toString() {
+     String array = "";
+     if (size == 0 ) array = "[]";
+     for (int i=0; i<data.length; i++) {
+       if (i == size - 1) {
+         array+=String.valueOf(data[i])+"]";
+       }
+       else array+=String.valueOf(data[i])+", ";
+     }
+     return array;
+   }
+
+  public boolean contains(String s) {
+    boolean a = false;
+    for (int i=0; i<data.length; i++) {
+      if (data[i].equals(s)) a = true;
+    }
+    return a;
   }
 }
