069e9c566d978c0f1bdfca7d56458f77b71032b7
ava-lu <alu21@stuy.edu>
2020-11-08 13:16:32 -0500

    added zip()

--- Demo.java
+++ Demo.java
@@ -18,6 +18,29 @@ public class Demo {
     return c;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b) {
+    SuperArray arr = new SuperArray();
+    if (a.size() >= b.size()) {
+      for (int i=0; i<b.size(); i++) {
+        arr.add(a.get(i));
+        arr.add(b.get(i));
+      }
+      for (int i=b.size(); i<a.size(); i++) {
+        arr.add(a.get(i));
+      }
+    }
+    if (b.size() > a.size()) {
+      for (int i=0; i<a.size(); i++) {
+        arr.add(a.get(i));
+        arr.add(b.get(i));
+      }
+      for (int i=a.size(); i<b.size(); i++) {
+        arr.add(b.get(i));
+      }
+    }
+    return arr;
+  }
+
   public static void main(String[] args) {
     //removeDuplicates test
     // SuperArray words = new SuperArray();
@@ -32,13 +55,13 @@ public class Demo {
     // System.out.println(words);
 
     //findOverlap test
-    SuperArray a = new SuperArray(6);
-    a.add("9"); a.add("1"); a.add("2");
-    a.add("2"); a.add("3"); a.add("4");
-    SuperArray b = new SuperArray(5);
-    b.add("0"); b.add("4"); b.add("2");
-    b.add("2"); b.add("9");
-    System.out.println(findOverlap(a, b));
+    // SuperArray a = new SuperArray(6);
+    // a.add("9"); a.add("1"); a.add("2");
+    // a.add("2"); a.add("3"); a.add("4");
+    // SuperArray b = new SuperArray(5);
+    // b.add("0"); b.add("4"); b.add("2");
+    // b.add("2"); b.add("9");
+    // System.out.println(findOverlap(a, b));
 
   }
 
