e161efb89271b13343d080ac920a6cddeabf5832
ava-lu <alu21@stuy.edu>
2020-11-08 11:10:55 -0500

    made a small correction

--- SuperArray.java
+++ SuperArray.java
@@ -84,7 +84,7 @@ public class SuperArray {
 
   public String remove(int index) {
     String value = data[index];
-    for (int i=index; i<size; i++) {
+    for (int i=index; i<size-1; i++) {
       data[i] = data[i+1];
     }
     size--;
