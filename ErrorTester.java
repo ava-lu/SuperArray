be0981daeaf4a8a1f07d5d0ea7dc4a945ac636d0
ava-lu <alu21@stuy.edu>
2020-11-11 11:57:40 -0500

    tested errors with set()

--- ErrorTester.java
+++ ErrorTester.java
@@ -10,15 +10,16 @@ public class ErrorTester {
     //   System.out.println("NEGATIVE CAPACITY??");
     // }
 
-    //get(int index)
-    // SuperArray test2 = new SuperArray(3);
-    // test2.add("a"); test2.add("b"); test2.add("c");
-    // try {
-    //   System.out.println(test2.get(10));
-    // }
-    // catch (IndexOutOfBoundsException e){
-    //   System.out.println("NOT IN BOUNDS!");
-    // }
+    //get() & set()
+    SuperArray test2 = new SuperArray(3);
+    test2.add("a"); test2.add("b"); test2.add("c");
+    try {
+    //  System.out.println(test2.get(10));
+      System.out.println(test2.set(-5, "yay"));
+    }
+    catch (IndexOutOfBoundsException e){
+      System.out.println("NOT IN BOUNDS!");
+    }
 
 
   }
