fcb582a8d85d4d274406c08fec91b8f1ac76b1c7
ava-lu <alu21@stuy.edu>
2020-11-11 12:02:11 -0500

    tested errors with add()

--- ErrorTester.java
+++ ErrorTester.java
@@ -10,12 +10,13 @@ public class ErrorTester {
     //   System.out.println("NEGATIVE CAPACITY??");
     // }
 
-    //get() & set()
+    //get() & set() & add()
     SuperArray test2 = new SuperArray(3);
     test2.add("a"); test2.add("b"); test2.add("c");
     try {
     //  System.out.println(test2.get(10));
-      System.out.println(test2.set(-5, "yay"));
+    //  System.out.println(test2.set(-5, "yay"));
+      test2.add(10319310, "yum");
     }
     catch (IndexOutOfBoundsException e){
       System.out.println("NOT IN BOUNDS!");
