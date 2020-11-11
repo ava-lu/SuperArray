82eec096e90187df9e704cbbca8e82455abc9ccf
ava-lu <alu21@stuy.edu>
2020-11-11 11:51:50 -0500

    tested errors with get()

--- ErrorTester.java
+++ ErrorTester.java
@@ -1,11 +1,25 @@
 public class ErrorTester {
   public static void main(String[] args) {
     //catch exceptions
-    try {
-      SuperArray test1 = new SuperArray(-5);
-    }
-    catch (IllegalArgumentException e) {
-      System.out.println("NEGATIVE CAPACITY??");
-    }
+
+    //SuperArray(int InitialCapacity)
+    // try {
+    //   SuperArray test1 = new SuperArray(-5);
+    // }
+    // catch (IllegalArgumentException e) {
+    //   System.out.println("NEGATIVE CAPACITY??");
+    // }
+
+    //get(int index)
+    // SuperArray test2 = new SuperArray(3);
+    // test2.add("a"); test2.add("b"); test2.add("c");
+    // try {
+    //   System.out.println(test2.get(10));
+    // }
+    // catch (IndexOutOfBoundsException e){
+    //   System.out.println("NOT IN BOUNDS!");
+    // }
+
+
   }
 }
